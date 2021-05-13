ad2e358d7ef2a0edfaa5cfdcf2c1124dcdb5c7ac
themikegit <63319107+themikegit@users.noreply.github.com>
2021-05-13 16:09:42 +0200

    Update tabs.js

--- src/comp/tabs.js
+++ src/comp/tabs.js
@@ -14,9 +14,11 @@ export default function Tabs() {
         </Pills>
         <Content>
           <div style = {{ display: pill == "About" ? "block" : "none" }} >
-              <p>Graphic designer with 5+ years of experience in brand id and web design. Skilled at Adobe Creative Suite (Photoshop and Illustrator), Sketch, Figma... Starting my career at 99desgin, Freelancer.com, and then moving to Upwork I had the opportunity to work with more than 600 clients from all parts of the world.
+              <p>Junior Web Developer with one year of experience in building web apps mainly as a playground for learning
+new skills. Seeking an opportunity to extend my knowledge and implement solutions to meet business
+needs. Currently in process of learning Angular 2+.
                <br/> <br/>
-               Since last year my main preoccupation has been FrontEnd development because I always wanted to make my ideas live!:) I'm in process of learning Angular!
+               
               </p>
             
            
@@ -60,7 +62,7 @@ export default function Tabs() {
                       <h3>Freelancer.com</h3>
                       <h4>2015-2019</h4>
                       <p>
-                        Creating brands for various businesses.
+                        Creating visual identities for various businesses.
                       </p>
               
                     </li>
